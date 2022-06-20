7424eb776decaf8d446191c02c1b37f47003068e
KosoScythe <tomas.kosec97@gmail.com>
2022-06-20 22:24:44 +0200

    First commit with blank project
    Blank template with bootstrap and jQuery

---
+++ functions.php
@@ -0,0 +1,39 @@
+﻿<?php
+
+function include_jquery() {
+	//LOAD JQUERY
+	wp_deregister_script('jquery');
+	
+	wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-3.4.1.min.js', '', 1, true);
+}
+add_action('wp_enqueue_scripts', 'include_jquery');
+
+function load_stylesheets() {
+	//LOAD BOOTSRAP
+	wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+	wp_enqueue_style('bootstrap');
+	
+	//LOAD MY OWN STYLE - MUST BE SECOND OTHERWISE BOOTSTRAP WILL OVERWRITE MY OWN STYLE
+	wp_register_style('stylesheet', get_template_directory_uri() . '/style.css', array(), false, 'all');
+	wp_enqueue_style('stylesheet');
+}
+
+add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+function loadjs() {
+	wp_register_script('customjs', get_template_directory_uri() . '/scripts.js', '', 1, true);
+	wp_enqueue_script('customjs');
+} 
+
+add_action('wp_enqueue_scripts', 'loadjs');
+
+add_theme_support('menus');
+
+register_nav_menus(
+	array(
+		'top-menu' => __('Top Menu, theme'),
+		'footer-menu' => __('Footer Menu, theme'),
+	)
+);
+
+?>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="sk">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Koso i drevina</title>
+
+  
+	
+	<link rel="icon" href="<?php echo get_bloginfo('template_directory'); ?>/images/icon.png" sizes="32x32">
+	<?php wp_head();?>
+	
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+		
+	</head>
+<body>
